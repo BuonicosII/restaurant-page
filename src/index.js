a06f98596f444502caa972f49f738248edfaf79b
BuonicosII <118397101+BuonicosII@users.noreply.github.com>
2023-05-28 20:21:44 +0200

    Add content on index.js and a stylesheet

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-alert("How are you?");
+import './style.css';
+
+let header = document.createElement("div");
+let h1 = document.createElement("h1");
+h1.textContent = "Luigi's Arrosticini";
+
+header.appendChild(h1);
+
+document.getElementById("content").appendChild(header);
