fe93d5231439509dbce1906bf4f65aced6470915
BuonicosII <118397101+BuonicosII@users.noreply.github.com>
2023-05-30 20:50:01 +0200

    Add backgroung image

--- src/loadpage.js
+++ src/loadpage.js
@@ -21,16 +21,21 @@ function loadpage() {
   bodyDiv.setAttribute("id", "bodyDiv");
   let mainElementHolder = document.createElement("div");
   mainElementHolder.setAttribute("id", "mainElementHolder");
+  let h2 = document.createElement("h2");
+  h2.textContent = "Enjoy a taste of Abruzzo"
   let arrosticiniImage = new Image();
   arrosticiniImage.src = arrosticini;
-  
+  let h3 = document.createElement("h3");
+  h3.textContent = "Come visit us for a bite of italian wilderness";
   header.appendChild(h1);
   header.appendChild(pOne);
   header.appendChild(tabHolder);
   tabHolder.appendChild(homeTab);
   tabHolder.appendChild(menu);
   tabHolder.appendChild(contacts);
+  mainElementHolder.appendChild(h2);
   mainElementHolder.appendChild(arrosticiniImage);
+  mainElementHolder.appendChild(h3);
   bodyDiv.appendChild(mainElementHolder);
   document.getElementById("content").appendChild(header);
   document.getElementById("content").appendChild(bodyDiv);
