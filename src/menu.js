29fb8a0a997e9ba1f5b3195627064c3450899973
BuonicosII <118397101+BuonicosII@users.noreply.github.com>
2023-06-04 15:17:06 +0200

    Add first menu item in the menu tab

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,5 @@
+import arrosticiniDish from "./arrosticini-dish.jpg"
+
 function menu() {
     document.getElementById("home").removeAttribute("class", "selectedTab");
     document.getElementById("contacts").removeAttribute("class", "selectedTab");   
@@ -7,6 +9,17 @@ function menu() {
     document.getElementById("menu").setAttribute("class", "selectedTab");
     let menuDiv = document.createElement("div");
     menuDiv.setAttribute("id", "menuDiv");
+
+    let dishOne = document.createElement("div");
+    dishOne.setAttribute("class", "dish");
+    let photoDivOne = new Image();
+    photoDivOne.src = arrosticiniDish
+    let cardOne = document.createElement("div");
+    cardOne.textContent = "Arrosticini mini";
+    dishOne.appendChild(photoDivOne);
+    dishOne.appendChild(cardOne);
+
+    menuDiv.appendChild(dishOne);
     bodyDiv.appendChild(menuDiv);
 }
 
